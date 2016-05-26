6142e2be001c8a51c6e0ed04c60b2a2f8c96b829
squarlhan <squarlhan@gmail.com>
2016-05-25 21:46:38 -0500

    Frenquencis file can be merged into 1 2D array; normalize and scale added.

--- src/test/java/ccst/dl/Normalize.java
+++ src/test/java/ccst/dl/Normalize.java
@@ -15,20 +15,36 @@ import java.util.List;
 import java.util.Map;
 
 public class Normalize {
-	
+
 	private int yst;
 	private int ysp;
 	private int mst;
 	private int msp;
-	
-	
-	
+	private double[][] disfrqs;
+	private double[][] normalfrqs;
+
 	public Normalize(int yst, int ysp, int mst, int msp) {
 		super();
 		this.yst = yst;
 		this.ysp = ysp;
 		this.mst = mst;
 		this.msp = msp;
+	}
+
+	public double[][] getNormalfrqs() {
+		return normalfrqs;
+	}
+
+	public void setNormalfrqs(double[][] normalfrqs) {
+		this.normalfrqs = normalfrqs;
+	}
+
+	public double[][] getDisfrqs() {
+		return disfrqs;
+	}
+
+	public void setDisfrqs(double[][] disfrqs) {
+		this.disfrqs = disfrqs;
 	}
 
 	public int getYst() {
@@ -62,8 +78,8 @@ public class Normalize {
 	public void setMsp(int msp) {
 		this.msp = msp;
 	}
-	
-	public void dir2monthly(String topfile, String dailydir, String monthlydir){
+
+	public void dir2monthly(String topfile, String dailydir, String monthlydir) {
 		File meshfile = new File(topfile);
 		List<String> tops = new ArrayList<String>();
 		try {
@@ -73,26 +89,25 @@ public class Normalize {
 				String l = line.trim();
 				if (l.length() > 0) {
 					tops.add(l);
-					newsnum_daily2monthly(dailydir+l+".txt", monthlydir+l+".txt");
+					newsnum_daily2monthly(dailydir + l + ".txt", monthlydir + l + ".txt");
 				}
 			}
-		} catch ( IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
-	public void newsnum_daily2monthly(String dailyaddr, String monthlyaddr){
+	public void newsnum_daily2monthly(String dailyaddr, String monthlyaddr) {
 
-		HashMap<String, Integer> newsmap = new HashMap<String, Integer>(); 
+		HashMap<String, Integer> newsmap = new HashMap<String, Integer>();
 		try {
 			File meshfile = new File(dailyaddr);
 			InputStreamReader ir = new InputStreamReader(new FileInputStream(meshfile));
 			BufferedReader reader = new BufferedReader(ir);
 			BufferedWriter bw = new BufferedWriter(new FileWriter(monthlyaddr));
-			
+
 			for (String line = reader.readLine().trim(); line != null; line = reader.readLine()) {
 
 				if (line.trim().length() > 0) {
@@ -107,16 +122,16 @@ public class Normalize {
 
 				}
 			}
-			
+
 			for (Map.Entry<String, Integer> entry : newsmap.entrySet()) {
 				bw.write(entry.getKey() + "\t" + entry.getValue() + "\n");
 				bw.flush();
 			}
-			
+
 			ir.close();
 			reader.close();
 			bw.close();
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -124,30 +139,29 @@ public class Normalize {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public HashMap<String, Integer> readmontlynums(String monthlyaddr){
-		HashMap<String, Integer> newsmap = new HashMap<String, Integer>(); 
+
+	public HashMap<String, Integer> readmontlynums(String monthlyaddr) {
+		HashMap<String, Integer> newsmap = new HashMap<String, Integer>();
 		try {
 			File meshfile = new File(monthlyaddr);
 			InputStreamReader ir = new InputStreamReader(new FileInputStream(meshfile));
 			BufferedReader reader = new BufferedReader(ir);
-			
+
 			for (String line = reader.readLine().trim(); line != null; line = reader.readLine()) {
 
 				if (line.trim().length() > 0) {
 					String[] lines = line.trim().split("\t");
 					String key = lines[0].trim().substring(0, 7);
-					int num = Integer.parseInt(lines[1].trim());					
+					int num = Integer.parseInt(lines[1].trim());
 					newsmap.put(key, num);
 				}
 			}
-			
-			
+
 			ir.close();
 			reader.close();
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -158,16 +172,127 @@ public class Normalize {
 		return newsmap;
 	}
 
+	public double[][] getmatrixfromfils(String topaddr, String diraddr) {
+		List<HashMap<String, Integer>> list = new ArrayList<HashMap<String, Integer>>();
+		try {
+			InputStreamReader ir = new InputStreamReader(new FileInputStream(topaddr));
+			BufferedReader reader = new BufferedReader(ir);
+			for (String line = reader.readLine().trim(); line != null; line = reader.readLine()) {
+				String l = line.trim();
+				if (l.length() > 0) {
+					list.add(readmontlynums(diraddr + l + ".txt"));
+				}
+			}
+			ir.close();
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int ii = list.size();
+		int jj = list.get(0).size();
+		double[][] re = new double[ii][jj];
+		for (int i = 0; i <= ii - 1; i++) {
+			int j = 0;
+			for (int y = yst; y <= ysp; y++) {
+				for (int m = 1; m <= 12; m++) {
+					if (!((y == yst && m < mst) || (y == ysp && m > msp))) {
+						String sy = String.valueOf(y);
+						String sm = String.valueOf(m);
+						sm = sm.length() < 2 ? "-0" + sm : "-" + sm;
+						re[i][j] = list.get(i).get(sy + sm);
+						j++;
+					}
+
+				}
+
+			}
+		}
+		this.disfrqs = re;
+		return re;
+
+	}
+	
+	public double[][] normal(String newsmonth){
+		int ii = disfrqs.length;
+    	int jj = disfrqs[0].length;
+    	double[][] output = new double[ii][jj];
+    	HashMap<String, Integer> numm = readmontlynums(newsmonth);
+    	for (int i = 0; i <= ii - 1; i++) {
+			int j = 0;
+			for (int y = yst; y <= ysp; y++) {
+				for (int m = 1; m <= 12; m++) {
+					if (!((y == yst && m < mst) || (y == ysp && m > msp))) {
+						String sy = String.valueOf(y);
+						String sm = String.valueOf(m);
+						sm = sm.length() < 2 ? "-0" + sm : "-" + sm;
+						output[i][j] = disfrqs[i][j] / numm.get(sy + sm);
+						j++;
+					}
+				}
+			}
+		}
+    	this.normalfrqs = scalebyx(0,1,output);
+    	return normalfrqs;
+	}
+	
+	public double[][] scalebyx(double lower, double upper, double[][] input){
+    	int m = input.length;
+    	int n = input[0].length;
+    	double[] min = new double[m];
+    	double[] max = new double[m];
+    	double[][] output = new double[m][n];
+    	for(int i = 0;i<=m-1;i++){
+    		min[i] = Double.MAX_VALUE;
+    		max[i] = -Double.MAX_VALUE;
+    		for(int j = 0;j<=n-1;j++){
+    			min[i] = Math.min(min[i],input[i][j]);
+    			max[i] = Math.max(max[i],input[i][j]);
+    		}
+    	}
+    	for(int i = 0;i<=m-1;i++){
+    		for(int j = 0;j<=n-1;j++){
+    			output[i][j] = ((upper-lower)*(input[i][j]-min[i])/(max[i]-min[i]))+lower;
+    		}
+    	}
+    	return output;
+    }
+	
+    public double[][] scalebyy(double lower, double upper, double[][] input){
+    	int m = input.length;
+    	int n = input[0].length;
+    	double[] min = new double[n];
+    	double[] max = new double[n];
+    	double[][] output = new double[m][n];
+    	for(int j = 0;j<=n-1;j++){
+    		min[j] = Double.MAX_VALUE;
+    		max[j] = -Double.MAX_VALUE;
+    		for(int i = 0; i<=m-1; i++){
+    			min[j] = Math.min(min[j],input[i][j]);
+    			max[j] = Math.max(max[j],input[i][j]);
+    		}
+    	}
+    	for(int j = 0;j<=n-1;j++){
+    		for(int i = 0; i<=m-1; i++){
+    			output[i][j] = ((upper-lower)*(input[i][j]-min[j])/(max[j]-min[j]))+lower;
+    		}
+    	}
+    	return output;
+    }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Normalize normal = new Normalize(2010, 2016, 4, 4);
-//		normal.newsnum_daily2monthly("C:/Users/install/Desktop/hxs/bbc/bbcdata/newsnum.txt", 
-//				"C:/Users/install/Desktop/hxs/bbc/bbcdata/newsmonth.txt");
-//		normal.readmontlynums("C:/Users/install/Desktop/hxs/bbc/bbcdata/newsmonth.txt");
-		normal.dir2monthly("C:/Users/install/Desktop/hxs/bbc/MeSH/top150.txt", 
-				"C:/Users/install/Desktop/hxs/bbc/bbcdata/allfrqs/", 
+		// normal.newsnum_daily2monthly("C:/Users/install/Desktop/hxs/bbc/bbcdata/newsnum.txt",
+		// "C:/Users/install/Desktop/hxs/bbc/bbcdata/newsmonth.txt");
+		// normal.readmontlynums("C:/Users/install/Desktop/hxs/bbc/bbcdata/newsmonth.txt");
+		// normal.dir2monthly("C:/Users/install/Desktop/hxs/bbc/MeSH/top150.txt",
+		// "C:/Users/install/Desktop/hxs/bbc/bbcdata/allfrqs/",
+		// "C:/Users/install/Desktop/hxs/bbc/bbcdata/topfrqs/");
+		normal.getmatrixfromfils("C:/Users/install/Desktop/hxs/bbc/MeSH/top150.txt",
 				"C:/Users/install/Desktop/hxs/bbc/bbcdata/topfrqs/");
+		normal.normal("C:/Users/install/Desktop/hxs/bbc/bbcdata/newsmonth.txt");
 	}
 
 }
